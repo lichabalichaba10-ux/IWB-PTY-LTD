a864f0a94f5e5483c1cdd58cf0405f78ef19d964
lichabalichaba10-ux <lichabalichaba10@gmail.com>
2025-11-06 13:27:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.querySelector('nav ul');
+
+
+menuToggle.addEventListener('change', () => {
+  if (menuToggle.checked) {
+    navMenu.classList.add('open');
+  } else {
+    navMenu.classList.remove('open');
+  }
+});
